ba7d872bafde1f16c06f2fd73ab500b06861bafe
jakekato <loveillusion02@gmail.com>
2024-01-25 05:21:19 +0700

    first commit

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+void solve() { 
+    float a = 4.5;
+    int b = 4;
+    if ( (int)a > b ) {
+        cout << "yes";
+    } else { cout << "no"; }
+}
+
+int main() { 
+	//freopen("input.txt", "r", stdin);
+	//freopen("error.txt", "w", stderr);
+
+	ios_base::sync_with_stdio(false);cin.tie(NULL);
+	solve();
+	return 0;
+}
